63152ae21c7058cd180dd3f00682df0dac19b862
willy <willy.yudithya@pegipegi.com>
2021-01-11 13:07:25 +0700

    Support format option

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const options = yargs
 .default('c', 'ASII, BBCA, TLKM, INDF, UNVR' , 'Show top 20 IDX codes')
 .usage('Usage: -c <code>')
 .option('c', { alias: 'code', describe: 'IDX code (eg: BEN or multiple codes BEN,CBA,ATP)', type: 'string', demandOption: false })
+.option('f', { alias: 'format', describe: 'format output (eg: price,change,volume)', type: 'string', demandOption: false })
 .argv;
 
 let code = `${options.code}`.split(',');
 
 let codes = [], size = 10;
 
+let format = `${options.format}`;
 while (code.length > 0) {
   codes.push(code.splice(0, size));
 }
@@ -26,7 +28,7 @@ let results = [];
 
 console.clear();
 
-function getData(idxCodes) {
+function getData(idxCodes, $raw) {
   return new Promise((resolve, reject) => {
  
     idxCodes = idxCodes.trim();
@@ -36,16 +38,28 @@ function getData(idxCodes) {
    
       var data = JSON.parse(body);
       var change = data['Change'] >= 0 ? false: true ;
-     
-      results.push({
-            code: chalk.bgHex('#333').hex('#fff').bold(idxCodes),
-            price: chalk.hex('#fff')(data['Price']),
-            '$+/-': change  ? chalk.hex('#e88388').bold(data['Change']) : chalk.hex('#a8cc8b').bold(data['Change']),
-            '% chg': change  ? chalk.hex('#e88388').bold(data['Percent']) : chalk.hex('#a8cc8b').bold(data['Percent']),            
-            value: chalk.hex('#fff')(data['Value']),
-            frequency: chalk.hex('#fff')(data['Frequency']),
-            volume: chalk.hex('#fff')(data['Volume'])
-      });
+    
+      if($raw==false){
+        results.push({
+              code: chalk.bgHex('#333').hex('#fff').bold(idxCodes),
+              price: chalk.hex('#fff')(data['Price']),
+              '$+/-': change  ? chalk.hex('#e88388').bold(data['Change']) : chalk.hex('#a8cc8b').bold(data['Change']),
+              '% chg': change  ? chalk.hex('#e88388').bold(data['Percent']) : chalk.hex('#a8cc8b').bold(data['Percent']),            
+              value: chalk.hex('#fff')(data['Value']),
+              frequency: chalk.hex('#fff')(data['Frequency']),
+              volume: chalk.hex('#fff')(data['Volume'])
+        });
+      }else{
+        results.push({
+              code: idxCodes,
+              price: data['Price'],
+              '$+/-':data['Change'] ,
+              '% chg': data['Percent'],            
+              value: data['Value'],
+              frequency: data['Frequency'] ,
+              volume: data['Volume'] 
+        });
+      }
   
       resolve(results);
     })
@@ -57,7 +71,12 @@ function getData(idxCodes) {
 const getAllData = async () => {
   return Promise.all(codes[0].map(codes => {
     const idxCodes = codes.toString().replace(/,/g, '+');
-    return getData(idxCodes)
+    
+    if (typeof format == 'undefined'){
+      return getData(idxCodes, false);
+    }else{
+      return getData(idxCodes, true);
+    }
   }))
 }
 
@@ -75,10 +94,22 @@ getAllData().then((val) => {
     }
   }
   const columns = columnify(results, tableOptions);
-  console.log(chalk.hex('#fff').bold(`\n$$$ IDX Ticker $$$`));
-  console.log(divider);
-  if (results.length) {
-    console.log(columns);
-    console.log(`${divider}\n* prices are delayed up to 20 minutes\n`);
+  if (typeof format == 'undefined'){
+    if (results.length) {
+      console.log(chalk.hex('#fff').bold(`\n$$$ IDX Ticker $$$`));
+      console.log(columns);
+      console.log(divider);
+      console.log(`${divider}\n* prices are delayed up to 20 minutes\n`);
+    }
+  }else{
+    if (results.length){
+      if(format == 'price'){
+        results.forEach(function(result, index){
+          if (typeof result.price  != 'undefined'){
+            console.log(result.price);
+          }
+        });
+      }
+    }
   }
 });
